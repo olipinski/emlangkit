ab98eec5549804d56cfe827e4e79fb644296122a
Olaf Lipinski <o.lipinski@soton.ac.uk>
2023-11-06 13:57:31 +0000

    Add M_previous^n metric
    Signed-off-by: Olaf Lipinski <o.lipinski@soton.ac.uk>

--- emlangkit/metrics/__init__.py
+++ emlangkit/metrics/__init__.py
@@ -1,6 +1,7 @@
 """Root __init__ of the metrics."""
 from emlangkit.metrics.bosdis import compute_bosdis
 from emlangkit.metrics.entropy import compute_entropy
+from emlangkit.metrics.mpn import compute_mpn
 from emlangkit.metrics.mutual_information import compute_mutual_information
 from emlangkit.metrics.posdis import compute_posdis
 from emlangkit.metrics.topsim import compute_topographic_similarity
@@ -12,4 +13,5 @@ __all__ = [
     "compute_mutual_information",
     "compute_posdis",
     "compute_topographic_similarity",
+    "compute_mpn",
 ]
--- tests/test_metrics.py
+++ tests/test_metrics.py
@@ -501,3 +501,227 @@ def test_bosdis():
         1.0,
         2,
     )
+
+
+def test_mpn():
+    """Tests to see if M_previous^n is calculated correctly."""
+    test_obs = np.array([[x, y] for x in range(4) for y in range(4)])
+
+    np.testing.assert_almost_equal(
+        metrics.compute_mpn(
+            messages=np.array(
+                [
+                    [0, 0, 0],
+                    [0, 0, 1],
+                    [0, 0, 2],
+                    [0, 0, 3],
+                    [0, 1, 0],
+                    [0, 1, 1],
+                    [0, 1, 2],
+                    [0, 1, 3],
+                    [2, 0, 0],
+                    [2, 0, 1],
+                    [2, 0, 2],
+                    [2, 0, 3],
+                    [2, 1, 0],
+                    [2, 1, 1],
+                    [2, 1, 2],
+                    [2, 1, 3],
+                ]
+            ),
+            observations=test_obs,
+            prev_horizon=8,
+        )[0],
+        0,
+        2,
+    )
+
+    # noinspection PyTypeChecker
+    np.testing.assert_almost_equal(
+        metrics.compute_mpn(
+            messages=np.array(
+                [
+                    [1],
+                    [2],
+                    [2],
+                    [2],
+                ]
+            ),
+            observations=np.array(
+                [
+                    [4],
+                    [4],
+                    [4],
+                    [4],
+                ]
+            ),
+            prev_horizon=8,
+        )[1],
+        100,
+        2,
+    )
+
+    # noinspection PyTypeChecker
+    np.testing.assert_almost_equal(
+        metrics.compute_mpn(
+            messages=np.array(
+                [
+                    [1],
+                    [2],
+                    [2],
+                    [2],
+                ]
+            ),
+            observations=np.array(
+                [
+                    [4],
+                    [4],
+                    [4],
+                    [4],
+                ]
+            ),
+            prev_horizon=8,
+        )[2],
+        0,
+        2,
+    )
+
+    # noinspection PyTypeChecker
+    np.testing.assert_almost_equal(
+        metrics.compute_mpn(
+            messages=np.array(
+                [
+                    [0, 1],
+                    [0, 1],
+                    [0, 1],
+                    [0, 1],
+                    [4, 1],
+                    [2, 1],
+                    [4, 1],
+                    [3, 1],
+                    [1, 1],
+                ]
+            ),
+            observations=np.array(
+                [
+                    [1],
+                    [2],
+                    [3],
+                    [4],
+                    [1],
+                    [4],
+                    [3],
+                    [1],
+                    [1],
+                ]
+            ),
+            prev_horizon=8,
+        )[1],
+        100,
+        2,
+    )
+
+    # noinspection PyTypeChecker
+    np.testing.assert_almost_equal(
+        metrics.compute_mpn(
+            messages=np.array(
+                [
+                    [0, 1],
+                    [0, 1],
+                    [0, 1],
+                    [0, 1],
+                    [4, 1],
+                    [2, 1],
+                    [4, 1],
+                    [3, 1],
+                    [1, 1],
+                ]
+            ),
+            observations=np.array(
+                [
+                    [1],
+                    [2],
+                    [3],
+                    [4],
+                    [1],
+                    [4],
+                    [3],
+                    [1],
+                    [1],
+                ]
+            ),
+            prev_horizon=8,
+        )[2],
+        100,
+        2,
+    )
+
+    # noinspection PyTypeChecker
+    np.testing.assert_almost_equal(
+        metrics.compute_mpn(
+            messages=np.array(
+                [
+                    [0, 1],
+                    [0, 1],
+                    [0, 1],
+                    [0, 1],
+                    [4, 1],
+                    [2, 1],
+                    [4, 1],
+                    [3, 1],
+                    [1, 1],
+                ]
+            ),
+            observations=np.array(
+                [
+                    [1],
+                    [2],
+                    [3],
+                    [4],
+                    [1],
+                    [4],
+                    [3],
+                    [1],
+                    [1],
+                ]
+            ),
+            prev_horizon=8,
+        )[3],
+        100,
+        2,
+    )
+
+    # noinspection PyTypeChecker
+    np.testing.assert_almost_equal(
+        metrics.compute_mpn(
+            messages=np.array(
+                [
+                    [0, 1],
+                    [0, 1],
+                    [0, 1],
+                    [0, 1],
+                    [4, 1],
+                    [2, 1],
+                    [4, 1],
+                    [3, 1],
+                    [1, 1],
+                ]
+            ),
+            observations=np.array(
+                [
+                    [1],
+                    [2],
+                    [3],
+                    [4],
+                    [1],
+                    [4],
+                    [3],
+                    [1],
+                    [1],
+                ]
+            ),
+            prev_horizon=8,
+        )[4],
+        100,
+        2,
+    )
